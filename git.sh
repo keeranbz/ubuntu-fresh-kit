451cb4453e47e96800eceb52e90996f4a5967d57
Keeran <email>
2024-02-28 17:27:12 +0200

    hugo

---
+++ git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Installing git..."
+
+# Install
+apt-get -qq install git
--- hugo.sh
+++ hugo.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "Installing Hugo..."
+echo "Installing hugo..."
+
+# Dependencies
+sh ./git.sh
