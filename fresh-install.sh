f025516e7dd3abede59a3b3072cb077b82b1abb8
Keeran Bezuidenhout <32715219+keeranbz@users.noreply.github.com>
2024-02-29 02:00:06 +0200

    Update fresh-install.sh

--- fresh-install.sh
+++ fresh-install.sh
@@ -11,9 +11,8 @@ sudo apt-get -qq install python3 python-is-python3
 
 # yt-dlp
 echo "Installing yt-dlp"
-wget -q https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O $HOME/.local/bin/yt-dlp
+wget -qN https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O $HOME/.local/bin/yt-dlp
 chmod a+rx $HOME/.local/bin/yt-dlp  # Make executable
-yt-dlp -U
 
 # end
 echo "Done"
